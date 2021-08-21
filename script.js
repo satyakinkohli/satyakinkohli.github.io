95b476489df38add044f24229530993982c48aa4
satyakinkohli <satyakinkohli@gmail.com>
2021-08-21 17:01:19 +0530

    final

---
+++ script.js
@@ -0,0 +1,7 @@
+function hover(element) {
+    element.setAttribute('src', 'photos/memoji-thumbsup-white.jpeg');
+}
+  
+function unhover(element) {
+    element.setAttribute('src', 'photos/memoji-white.jpeg');
+}
