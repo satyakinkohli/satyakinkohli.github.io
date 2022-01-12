67db0d4e48d5470484e56e414fc544ad96221725
satyakinkohli <satyakinkohli@gmail.com>
2022-01-12 17:55:13 +0530

    mobile responsiveness added

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// function hover(element) {
-//     element.setAttribute('src', 'photos/memoji-thumbsup-white.jpeg');
-// }
-  
-// function unhover(element) {
-//     element.setAttribute('src', 'photos/memoji-white.jpeg');
-// }
-
 function hover(element) {
     if ($(window).width() < 481) {
         element.setAttribute('src', 'photos/memoji-white.jpeg');
