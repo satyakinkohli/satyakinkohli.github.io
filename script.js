301c2991bb0c8bdeb8dcc98b0dec9fc3ef884687
satyakinkohli <satyakinkohli@gmail.com>
2022-01-12 17:39:36 +0530

    mobile responsiveness check

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-function hover(element) {
-    element.setAttribute('src', 'photos/memoji-thumbsup-white.jpeg');
-}
+// function hover(element) {
+//     element.setAttribute('src', 'photos/memoji-thumbsup-white.jpeg');
+// }
   
-function unhover(element) {
-    element.setAttribute('src', 'photos/memoji-white.jpeg');
+// function unhover(element) {
+//     element.setAttribute('src', 'photos/memoji-white.jpeg');
+// }
+
+if ($(window).width() < 481) {
+    function hover(element) {
+        element.setAttribute('src', 'photos/memoji-white.jpeg');
+    }
+      
+    function unhover(element) {
+        element.setAttribute('src', 'photos/memoji-white.jpeg');
+    }
+} else {
+    function hover(element) {
+        element.setAttribute('src', 'photos/memoji-thumbsup-white.jpeg');
+    }
+      
+    function unhover(element) {
+        element.setAttribute('src', 'photos/memoji-white.jpeg');
+    }
 }
